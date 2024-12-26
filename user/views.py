999f6fdf872a50461d017ec6ffef64a76fb65f95
rolan <rolanakzoltoj@gmail.com>
2024-12-26 17:47:00 +0600

    hw5

--- user/views.py
+++ user/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
     
 
 
-
 def register_view(request):
     if request.method == "GET":
         form = RegisterForm()
@@ -42,7 +41,7 @@ def login_view(request):
                 return render(request, "user/login.html", context={"form": form})
             
 
-            
+
 @login_required(login_url="login-view")
 def logout_view(request):
     logout(request)
